684545eff5855cdbba481288c89bee7683488b29
Andrei Hîncu <51757656+andreihincusymphony@users.noreply.github.com>
2020-05-10 16:13:55 +0200

    Create Issue.java

---
+++ Issue.java
@@ -0,0 +1,20 @@
+package com.checkmarx.flow.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+import java.util.Map;
+
+public class Issue {
+
+    private String id;
+    private String url;
+    private String title;
+    private String body;
+    private String state;
+    private List<String> labels;
+    private Map<String, String> metadata;
+}
